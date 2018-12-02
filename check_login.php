aba8605794c7517504b4dda610525e46cec10c38
zhangyi <2998658517@qq.com>
2018-12-02 10:38:23 +0800

    Update check_login.php

--- check_login.php
+++ check_login.php
@@ -3,7 +3,7 @@
 include_once 'wx/config.php';
 $scene_id = isset($_POST['scene_id']) ? intval($_POST['scene_id']) : "";
 if ($scene_id > 0) {
-    $sql = "SELECT openid,nickname,is_first FROM `qrcode` WHERE `id` =" . $scene_id . "";
+    $sql = "SELECT openid,nickname,is_first FROM `qrcode` WHERE `id` =" . $scene_id;
     $query = mysqli_query($link,$sql);
     $arr = mysqli_fetch_array($query);
     echo json_encode($arr);
